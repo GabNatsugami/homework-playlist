8e02ce0e914372ad6adcd6a21fae85db7dfc7507
WolfyKunn <wolfybwox@gmail.com>
2022-05-15 19:40:38 +0300

    Zoomed to 150%

--- script.js
+++ script.js
@@ -241,3 +241,4 @@ var app = new Vue({
   const randomColor = "#"+((1<<24)*Math.random()|0).toString(16); 
 
   document.documentElement.style.setProperty('--main-bg-color', randomColor);
+  document.body.style.zoom="150%";
